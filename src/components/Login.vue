439332f0d40ff27898331349832c9e5d335565b5
devil19400 <devil19400@gmail.com>
2023-09-27 09:23:00 +0700

    frontend

---
+++ src/components/Login.vue
@@ -0,0 +1,71 @@
+<template>
+<v-card>
+  <v-card-title style="font-size: 24px !important;">เข้าสู่ระบบ</v-card-title>
+  <v-card-text>
+  <v-form
+    ref="loginform"
+    v-model="valid"
+    lazy-validation
+  >
+    <v-text-field
+      v-model="name"
+      :counter="20"
+      :rules="nameRules"
+      label="ชื่อผู้ใช้งาน"
+      required
+      outlined
+    ></v-text-field>
+
+    <v-text-field
+      v-model="password"
+      :rules="passwordRules"
+      label="รหัสผ่าน"
+      required
+      outlined
+    ></v-text-field>
+
+    <v-btn
+      :disabled="!valid"
+      color="success"
+      class="mr-4"
+      @click="Login()"
+      block
+    >
+      เข้าสู่ระบบ
+    </v-btn>
+
+  </v-form>
+  </v-card-text>
+</v-card>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    valid: true,
+    name: '',
+    nameRules: [
+      v => !!v || 'กรุณากรอกชื่อผู้ใช้งาน',
+      v => (v && v.length <= 20) || 'กรุณากรอกชื่อผู้ใช้งานห้ามเกิน 20 ตัวอักษร'
+    ],
+    password: '',
+    passwordRules: [
+      v => !!v || 'กรุณากรอกรหัสผ่าน'
+    ]
+  }),
+
+  methods: {
+    Login () {
+      if (this.$refs.loginform.validate(true)) {
+        localStorage.setItem('username', this.name)
+        this.$EventBus.$emit('getUsername')
+        this.$$router.push('/')
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,16 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/LoginView.vue')
+  },
+  {
+    path: '/manageTable',
+    name: 'manageTable',
+    component: () => import('../views/TableView.vue')
   }
 ]
 
